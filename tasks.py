27bd721904f7e82803a4eeea62855c913c53dec8
Mario Quiroga <marioqui135@gmail.com>
2023-08-14 18:21:36 +0200

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,6 +1,8 @@
 from robocorp.storage import get_storage_item
 import logging
 
+asset_name = "robocorp-awesome"
+
 def log_asset_to_console(asset_name):
     asset = get_storage_item(asset_name)
     logging.info(asset)
